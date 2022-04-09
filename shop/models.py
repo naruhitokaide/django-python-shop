d8385f39e3881a6f4cc91454c5e52fdaf8cae582
KodaKDominus <kodakatsuro@gmail.com>
2022-04-09 16:35:02 +0600

    added brand model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from django.db import models
 
 class Category(models.Model):
@@ -9,3 +10,19 @@ class Category(models.Model):
     class Meta:
         verbose_name = "Category"
         verbose_name_plural = "Categories"
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+    
+        
+        
+# class Product(models.Model):
+#     name = models.CharField(max_length=200)
+#     price = models.FloatField()
+#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+#     brand_name = models.ForeignKey()
