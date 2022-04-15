d5ebdd6bc3b1cf876999d16594c998749aabc469
KodaKDominus <kodakatsuro@gmail.com>
2022-04-15 15:53:45 +0600

    route for users app

--- Django_Shop/urls.py
+++ Django_Shop/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('user', include('users.urls')),
     path('', include('shop.urls')),
 ]
 
