f27fb1d960ba15c79a6c2d477804449ec87f688d
KodaKDominus <kodakatsuro@gmail.com>
2022-04-15 17:57:58 +0600

    user registration

--- users/views.py
+++ users/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 def registration(request):
+    if request.POST:
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+            return redirect('home')
+        else:
+            return render(request, 'registration.html', {'form':form})
+    else:
+        print('get request')
     return render(request, 'registration.html', {})
     
