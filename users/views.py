65287f3fbda912c55da4047341ef7f0123716dc6
KodaKDominus <kodakatsuro@gmail.com>
2022-04-15 19:42:52 +0600

    Automatic login after registration

--- users/views.py
+++ users/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
 def registration(request):
     if request.POST:
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}')
+            
             return redirect('home')
         else:
             return render(request, 'registration.html', {'form':form})
