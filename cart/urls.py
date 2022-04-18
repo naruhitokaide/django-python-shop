b67a68f02bfa40305d65860be31ee6fd257afce7
KodaKDominus <kodakatsuro@gmail.com>
2022-04-19 02:19:13 +0600

    cart

--- cart/urls.py
+++ cart/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('/add', views.add_cart, name='add-to-cart')
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def add_cart(request):
+    return HttpResponse(request.COOKIES['device'])
