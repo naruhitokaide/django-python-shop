72de1a5ced0188f394aac52bf9c8e7b2992fae37
KodaKDominus <kodakatsuro@gmail.com>
2022-04-20 16:31:02 +0600

    urls for add to cart

--- order/views.py
+++ order/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Cart
+
+def cart(request, id):
+    if request.POST:
+        if Cart.objects.filter(device = request.COOKIES['device']).exists():
+            return HttpResponse("Not Valid")
+        else:
+            return HttpResponse("Valid")
+    return HttpResponse(cart)
+        
