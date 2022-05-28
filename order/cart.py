f349bf5b685e80bafd7e60f8f53773edee4d0fdd
KodaKDominus <kodakatsuro@gmail.com>
2022-05-28 11:03:48 +0600

    cart session created

---
+++ order/cart.py
@@ -0,0 +1,12 @@
+
+
+class Cart():
+
+    def __init__(self, request):
+        self.session = request.session
+        cart = self.session.get('s_key')
+        if 's_key' not in request.session:
+            cart = self.session['s_key'] = {}
+        self.cart = cart
+
+
