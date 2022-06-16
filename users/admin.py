b3378e04f26727b4f2313825b44790d66a05d2e0
KodaKDominus <kodakatsuro@gmail.com>
2022-06-16 20:47:00 +0600

    Extended the existing User model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from users.models import UserInfo
+
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+class UserInLine(admin.StackedInline):
+    model = UserInfo
+    can_delete: False
+    verbose_name_plural = 'users'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserInLine,)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=13)
