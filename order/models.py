e143dea000a28b326e6b96f4a2f501dbc43bb0a8
KodaKDominus <kodakatsuro@gmail.com>
2022-06-17 21:09:44 +0600

    change to custom user from default user model

--- order/models.py
+++ order/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from shop.models import Product
-from django.contrib.auth.models import User
+
+from users.models import User
 
 class Cart(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
