7304f43562a243dfcfcbf78e28679b983a7d9087
KodaKDominus <kodakatsuro@gmail.com>
2022-06-17 21:10:48 +0600

    changed default user to custom user model import

--- users/models.py
+++ users/models.py
@@ -1,6 +1,62 @@
-from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-class UserInfo(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=13)
+from django.db import models
+from django_countries.fields import CountryField
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+
+    def create_superuser(self, email, user_name, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_stuff') is not True:
+            raise ValueError("Superuser must be assigned to 'is_stuff' = True")
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError("Superuser must be assigned to 'is_superuser' = True")
+
+        return self.create_user(email, user_name, password, **other_fields)
+
+    def create_user(self, email, user_name, password, **other_fields):
+        if not email:
+            raise ValueError(_('Please Provide Email Address'))
+        email = self.normalize_email(email)
+        user = self.model(email = email, user_name = user_name, **other_fields)
+
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique = True)
+    user_name = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    about = models.TextField(_(
+        'about'), max_length=500, blank=True)
+    # Delivery details
+    country = CountryField()
+    phone_number = models.CharField(max_length=15, blank=True)
+    postcode = models.CharField(max_length=12, blank=True)
+    address_line_1 = models.CharField(max_length=150, blank=True)
+    address_line_2 = models.CharField(max_length=150, blank=True)
+    town_city = models.CharField(max_length=150, blank=True)
+    # User Status
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['user_name']
+
+    class Meta:
+        verbose_name = "Accounts"
+        verbose_name_plural = "Accounts"
+
+
+
