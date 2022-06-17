a80c94163f641625fbf586c36908d02dfeedd2f3
KodaKDominus <kodakatsuro@gmail.com>
2022-06-17 21:09:20 +0600

    set new auth user model

--- Django_Shop/settings.py
+++ Django_Shop/settings.py
@@ -137,3 +137,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'asset')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
